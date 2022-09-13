1bba3feb71c15f1c521632e9e689609b4a647e56
Cameron Leech-Thomson <cameronleechthomson@gmail.com>
2022-09-13 02:59:21 +0100

    Zoom in/out
    bed time

--- D&D Encounter Visualiser/Assets/Scripts/RotateCamera.cs
+++ D&D Encounter Visualiser/Assets/Scripts/RotateCamera.cs
@@ -8,11 +8,13 @@ public class RotateCamera : MonoBehaviour
     public Transform target;
 
     private Vector3 center = Vector3.zero;
-
-    private bool shouldMove = true;
+    private Vector3 defaultPosition; 
+    private Vector3 editPosition = new Vector3(0, 20f, 0);
+    private bool shouldMove = false;
 
     void Start()
     {
+        defaultPosition = transform.position;
         if (target.childCount > 0){
             // Get center of all GameObjects:
             foreach (Transform child in target){
@@ -22,7 +24,9 @@ public class RotateCamera : MonoBehaviour
         } else{
             center = target.position;
         }
-        transform.LookAt(center);        
+        transform.LookAt(center);      
+        editPosition = new Vector3(center.x, 20f, center.z);
+        shouldMove = true;
     }
 
     // Update is called once per frame
@@ -31,11 +35,31 @@ public class RotateCamera : MonoBehaviour
         if (shouldMove) transform.RotateAround(center, Vector3.up, 25 * Time.deltaTime);
     }
 
+    private void zoom(float inc){
+        shouldMove = false;
+        defaultPosition = new Vector3(defaultPosition.x, defaultPosition.y, defaultPosition.z + inc);
+        transform.position = defaultPosition;
+        transform.LookAt(center); 
+        shouldMove = true;
+    }
+
+    public void zoomIn(){
+        zoom(5f);
+    }
+
+    public void zoomOut(){
+        zoom(-5f);
+    }
+
     public void pauseCamera(){
         shouldMove = false;
+        transform.position = editPosition;
+        transform.LookAt(center);
     }
 
     public void moveCamera(){
+        transform.position = defaultPosition;
+        transform.LookAt(center);
         shouldMove = true;
     }
 }
