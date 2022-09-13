2c7937b712b83c0f03f732858cff0f4bba4320ee
Cameron Leech-Thomson <cameronleechthomson@gmail.com>
2022-09-14 00:56:30 +0100

    Added anchors & snapping

--- DNDEC/Assets/Scripts/CreateAnchors.cs
+++ DNDEC/Assets/Scripts/CreateAnchors.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class CreateAnchors : MonoBehaviour
 {
 
-    private float angle = 60f;
-    private float distance = 2.1f;
+    public GameObject anchor;
+    public bool startWithAnchors = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private float angle = 60f;
+    private float distance = 2.165f;
+
+    void Start() {
+        if (startWithAnchors){
+            createAnchors();
+        }
+    }
+
+    public void createAnchors() {
+        for (int i = 0; i < 6; i++)
+        {
+            Vector3 pos = Quaternion.Euler(0f, angle * i, 0f) * (transform.forward * distance) + transform.position;
+
+            Collider[] hitColliders = Physics.OverlapSphere(pos, distance / 4, LayerMask.GetMask("Tile", "Anchor"));
+            if (hitColliders.Length == 0){
+                placeAnchor(pos);
+            }
+        }
+    }
+
+    void placeAnchor(Vector3 position){
+        GameObject obj = Instantiate(anchor, position, new Quaternion(0f, 0f, 0f, 1f)) as GameObject;
+        obj.GetComponent<SphereCollider>().radius = distance / 2;
     }
 
     // Update is called once per frame
--- DNDEC/Assets/Scripts/PlaceBlockFromClick.cs
+++ DNDEC/Assets/Scripts/PlaceBlockFromClick.cs
@@ -18,7 +18,7 @@ public class PlaceBlockFromClick : MonoBehaviour
     private void Start()
     {
         cam = Camera.main;
-        target = LayerMask.GetMask("Screencast Target", "Tile");
+        target = LayerMask.GetMask("Screencast Target", "Tile", "Anchor");
         foreach (Transform child in targetParent.transform){
             sceneObjects.Add(child);
         }
@@ -28,6 +28,7 @@ public class PlaceBlockFromClick : MonoBehaviour
         if (!placing){
             placing = true;
             placingObject = Instantiate(objectToPlace) as GameObject;
+            placingObject.transform.parent = targetParent.transform;
             placingObject.GetComponent<Renderer>().material = objectMaterial;
             (cam.GetComponent<RotateCamera>() as RotateCamera).pauseCamera();
         }        
@@ -35,11 +36,20 @@ public class PlaceBlockFromClick : MonoBehaviour
 
     private void Update() {
         if (placing){
+            bool canPlace = false;
+
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 100, target)){
-                if (hit.transform.gameObject.layer != LayerMask.NameToLayer("Tile")){
+                GameObject objectHit = hit.transform.gameObject;
+                // Snapping to Anchors:
+                if (objectHit.layer == LayerMask.NameToLayer("Anchor")){
+                    placingObject.transform.position = objectHit.transform.position;
+                    canPlace = true;
+                } // Stopping overlap: 
+                else if (objectHit.layer != LayerMask.NameToLayer("Tile")){
                     bool tooClose = false;
+                    canPlace = false;
                     foreach (Transform pos in sceneObjects){
                         if(Vector3.Distance(pos.position, hit.point) < 2.165f){
                             tooClose = true;
@@ -48,14 +58,16 @@ public class PlaceBlockFromClick : MonoBehaviour
                     if(!tooClose){
                         placingObject.transform.position = new Vector3(hit.point.x, 0f, hit.point.z); 
                     }         
-                }        
-            }
+                }
 
-            if(Input.GetKeyDown(KeyCode.Mouse0)){
-                placing = false;
-                placingObject.layer = LayerMask.NameToLayer("Tile");
-                placingObject = null;
-                (cam.GetComponent<RotateCamera>() as RotateCamera).moveCamera();
+                if(Input.GetKeyDown(KeyCode.Mouse0) && canPlace){
+                    placing = false;
+                    placingObject.layer = LayerMask.NameToLayer("Tile");
+                    placingObject.GetComponent<CreateAnchors>().createAnchors();
+                    placingObject = null;
+                    Destroy(objectHit);
+                    (cam.GetComponent<RotateCamera>() as RotateCamera).moveCamera();
+                }
             }
         }
     }
